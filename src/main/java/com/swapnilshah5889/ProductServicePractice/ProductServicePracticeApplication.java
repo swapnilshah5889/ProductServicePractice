a38c9958cbdefed0682c65a5c6f9ee0a322257a8
Swapnil Shah <swapnilshah5889@gmail.com>
2024-06-17 15:58:32 -0400

    Initialized Product Service

---
+++ src/main/java/com/swapnilshah5889/ProductServicePractice/ProductServicePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.swapnilshah5889.ProductServicePractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServicePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServicePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swapnilshah5889/ProductServicePractice/ProductServicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapnilshah5889.ProductServicePractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServicePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
