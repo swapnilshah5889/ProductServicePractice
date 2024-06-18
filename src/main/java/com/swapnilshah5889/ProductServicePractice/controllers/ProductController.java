80a8cdb17d39709ce998e330407d170c469721c8
Swapnil Shah <swapnilshah5889@gmail.com>
2024-06-17 21:49:51 -0400

    Implemented update and delete functions in controller

--- src/main/java/com/swapnilshah5889/ProductServicePractice/controllers/ProductController.java
+++ src/main/java/com/swapnilshah5889/ProductServicePractice/controllers/ProductController.java
@@ -9,18 +9,38 @@ import java.util.List;
 @RestController("/products")
 public class ProductController {
 
+    /* Get Product By ID */
     @GetMapping("/{id}")
     public Product getProduct(@PathVariable("id") Long id) {
         return new Product();
     }
 
+    /* Fetch All Products Product */
     @GetMapping
     public List<Product> getAllProducts() {
         return new ArrayList<>();
     }
 
+    /* Create New Product */
     @PostMapping
     public Product createProduct(@RequestBody Product product) {
         return new Product();
+    }
+
+    /* Partial Update Product */
+    @PatchMapping("/{id}")
+    public Product updateProduct(@PathVariable("id") Long id,@RequestBody Product product) {
+        return new Product();
+    }
+
+    /* Replace Product */
+    @PutMapping("/{id}")
+    public Product replaceProduct(@PathVariable("id") Long id,@RequestBody Product product) {
+        return new Product();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable("id") Long id) {
+
     }
 }
