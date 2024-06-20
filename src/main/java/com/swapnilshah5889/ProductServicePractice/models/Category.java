7c66a7d8fbeba2c384e18943562a4af93c84c0d0
Swapnil Shah <swapnilshah5889@gmail.com>
2024-06-20 13:49:02 -0400

    Added datasource url for jpa hibernate connection

--- src/main/java/com/swapnilshah5889/ProductServicePractice/models/Category.java
+++ src/main/java/com/swapnilshah5889/ProductServicePractice/models/Category.java
@@ -1,9 +1,13 @@
 package com.swapnilshah5889.ProductServicePractice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Category {
+    @Id
     private Long id;
     private String title;
 }
--- src/main/java/com/swapnilshah5889/ProductServicePractice/models/Product.java
+++ src/main/java/com/swapnilshah5889/ProductServicePractice/models/Product.java
@@ -1,17 +1,24 @@
 package com.swapnilshah5889.ProductServicePractice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product {
 
+    @Id
     private long id;
     private String title;
     private double price;
     private String description;
     private String image;
+
+    @ManyToOne
     private Category category;
 
 }
